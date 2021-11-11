cb55feb6a18f9dc7d84ab2906b7a5ff1006e1f61
nathenbyford <Nathenbyford@gmail.com>
2021-11-11 15:55:13 -0600

    Step 1 complete

---
+++ make things.r
@@ -0,0 +1,23 @@
+library("readxl")
+library("lubridate")
+library("tidyverse")
+theme_set(
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(),
+        axis.title.y = element_text(angle = 0, vjust = .5)
+  )
+)
+
+tb <- read_xlsx(".\\data\\data.xlsx", skip = 2)
+
+head(tb)
+dim(tb)
+
+dat_2 <- tb |> 
+  rename(county = "County Name") |> 
+  gather(-1, key = "date", value = "cases") |> 
+  mutate(date = str_extract(date, "[0-9]{2}-[0-9]{2}-[0-9]{4}"),
+         date = mdy(date))
+  
+
+head(dat_2)
